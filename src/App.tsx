6121f68899d16405916a45b62326e0095380df1b
Wayne5421 <caio.gabrielpdn@gmail.com>
2024-05-20 18:33:02 -0300

    fixed deploy bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { Footer } from './components/footer/Footer';
 import { Header } from './components/header/Header';
 import IncrementButton from './components/buy-button/Increment_button';
 
-interface Product {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  photo: string;
-}
+// interface Product {
+//   id: number;
+//   name: string;
+//   description: string;
+//   price: number;
+//   photo: string;
+// }
 
 function App() {
   const [number, setNumber] = useState(0);
